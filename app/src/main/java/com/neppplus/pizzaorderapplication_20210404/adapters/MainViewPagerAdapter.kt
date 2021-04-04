396ca428343b782d947813630d3090bd4193e870
전은형 <ooooo5h91@gmail.com>
2021-04-04 21:45:14 +0900

    메인화면에서 써먹는 뷰페이저 어댑터 작성 완료

--- app/src/main/java/com/neppplus/pizzaorderapplication_20210404/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/adapters/MainViewPagerAdapter.kt
@@ -6,8 +6,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.neppplus.pizzaorderapplication_20210404.fragments.MyProfileFragment
 import com.neppplus.pizzaorderapplication_20210404.fragments.PizzaStoreListFragment
 
-class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getItem(position: Int): Fragment {
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) { // 어댑터들을 관리해주는 폴더 생성 후 페이지어댑터 기본상속 후 생성자 fm 커스터마이팅했음. 그다음 필수 오버라이딩 함수 2개 불러내기
+    override fun getItem(position: Int): Fragment { // 2. 각 페이지에 맞게 표현해야하니까 리턴 웬 함수 사용
 
         return when (position) {
             0 -> PizzaStoreListFragment()
@@ -16,7 +16,7 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     }
 
-    override fun getCount(): Int {
+    override fun getCount(): Int { // 1. 몇장짜리인지 return 해주기
         return 2
     }
 
