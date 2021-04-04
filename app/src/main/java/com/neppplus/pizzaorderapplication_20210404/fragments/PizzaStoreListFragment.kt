da6e1f6ab1823c383f8967ab543b6f43de2764db
전은형 <ooooo5h91@gmail.com>
2021-04-04 21:40:57 +0900

    PizzaStoreListFragment 기초 주석 작성

--- app/src/main/java/com/neppplus/pizzaorderapplication_20210404/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/fragments/PizzaStoreListFragment.kt
@@ -13,20 +13,20 @@ import com.neppplus.pizzaorderapplication_20210404.adapters.StoreAdapter
 import com.neppplus.pizzaorderapplication_20210404.datas.Store
 import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
-class PizzaStoreListFragment : Fragment() {
+class PizzaStoreListFragment : Fragment() { // fragments 별도 패키지 만들고, ~Fragment 로 클래스 생성 후 Fragment()상속받게 하자 android x가 주는 걸로 선택
 
     val mPizzaStoreList = ArrayList<Store>()
     lateinit var mPizzaStoreAdapter : StoreAdapter
 
-    override fun onCreateView(
+    override fun onCreateView( // 어떤 xml 모양으로 쓸 건지 onCreateView 오버라이딩해서 불러주자
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false) // 어떤 xml이냐면 fragment_pizza_store_list
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) { //프래그먼트에서의 동작 관련된 코드들은 onActivityCreated 함수 오버라이딩해서 기록
         super.onActivityCreated(savedInstanceState)
 
         mPizzaStoreList.add(Store("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", "1588-5588"))
