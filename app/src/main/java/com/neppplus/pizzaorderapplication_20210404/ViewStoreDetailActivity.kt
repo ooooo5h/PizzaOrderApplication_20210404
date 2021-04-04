ed1041318e6d0c7a49cef02c9ae003978851580b
전은형 <ooooo5h91@gmail.com>
2021-04-04 15:11:43 +0900

    전화 연결 (+TedPermission) 작업

--- app/src/main/java/com/neppplus/pizzaorderapplication_20210404/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.pizzaorderapplication_20210404
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.neppplus.pizzaorderapplication_20210404.datas.Store
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -18,6 +24,29 @@ class ViewStoreDetailActivity : BaseActivity() {
 }
 
     override fun setUpEvents() {
+
+        callBtn.setOnClickListener {
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "연결이 거부되었습니다", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("[설정]에서 [전화권한]을 허용해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 
