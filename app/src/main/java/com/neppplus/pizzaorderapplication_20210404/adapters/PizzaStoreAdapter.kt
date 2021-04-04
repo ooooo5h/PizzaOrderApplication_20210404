3b922a5a08b0c575f61695d7c0abeb991f809b41
전은형 <ooooo5h91@gmail.com>
2021-04-04 13:56:59 +0900

    ListViewAdapter 뼈대 작업 진행

---
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/adapters/PizzaStoreAdapter.kt
@@ -0,0 +1,30 @@
+package com.neppplus.pizzaorderapplication_20210404.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.pizzaorderapplication_20210404.R
+import com.neppplus.pizzaorderapplication_20210404.datas.Store
+
+class PizzaStoreAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Store> ) : ArrayAdapter<Store>(mContext, resId, mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null) {
+
+            tempRow = inflater.inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
+}
