1adbdb738d25333a24abdfde90df868e2942eb9c
전은형 <ooooo5h91@gmail.com>
2021-04-04 13:49:34 +0900

    Store 클래스 추가 (가게 정보 표현)

---
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/datas/Store.kt
@@ -0,0 +1,9 @@
+package com.neppplus.pizzaorderapplication_20210404.datas
+
+import android.net.Uri
+
+class Store(
+    val name : String,
+    val logoUrl : String,
+    val phoneNum : String) {
+}
