7d5ecbe085505fde2299bc70ebfbeb6a29b50998
전은형 <ooooo5h91@gmail.com>
2021-04-04 21:57:32 +0900

    가게 정보 표현 클래스 추가

--- app/src/main/java/com/neppplus/pizzaorderapplication_20210404/datas/Store.kt
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/datas/Store.kt
@@ -3,7 +3,7 @@ package com.neppplus.pizzaorderapplication_20210404.datas
 import android.net.Uri
 import java.io.Serializable
 
-class Store(
+class Store( // 뿌려질 정보들을 datas 폴더에 만들어서 생성자 커스터마이징해줬음 - Serializable은 나중에
     val name : String,
     val logoUrl : String,
     val phoneNum : String) : Serializable {
