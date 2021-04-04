380297d939bf1b34042b9d152249601d77857bfe
전은형 <ooooo5h91@gmail.com>
2021-04-04 13:24:44 +0900

    MainViewPagerAdapter 작성

---
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.neppplus.pizzaorderapplication_20210404.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.pizzaorderapplication_20210404.fragments.MyProfileFragment
+import com.neppplus.pizzaorderapplication_20210404.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+
+        return when (position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
