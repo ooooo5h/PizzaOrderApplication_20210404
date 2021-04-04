181ac0dbc535edb67c0c3e372ca2035ddf1a9a48
전은형 <ooooo5h91@gmail.com>
2021-04-04 12:40:31 +0900

    만들어낸 BaseActivity 클래스 상속받아서 활용

--- app/src/main/java/com/neppplus/pizzaorderapplication_20210404/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapplication_20210404/MainActivity.kt
@@ -3,9 +3,23 @@ package com.neppplus.pizzaorderapplication_20210404
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+        setUpEvents()
+        setValues()
     }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+
 }
